e77177b23b564b688d903af4ffded852baf0793d
yoggy <yoggy0@gmail.com>
2015-06-09 15:28:37 +0900

    initial commit

---
+++ test01/Assets/scripts/fps.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class fps : MonoBehaviour {
+
+	public Text fpsText;
+
+	int count = 0;
+	float dulation = 3.0f;
+	float t;
+
+	void Start () {
+		reset ();
+	}
+
+	void reset() {
+		count = 0;
+		t = dulation;
+	}
+
+	void Update () {
+		t -= Time.deltaTime;
+		count++;
+
+		if (t <= 0) {
+			fpsText.text = "fps:" + count / dulation;
+			reset ();
+		}
+	}
+}
---
+++ test01/Assets/scripts/rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		transform.Rotate (1, 2, 3);
+	}
+}
