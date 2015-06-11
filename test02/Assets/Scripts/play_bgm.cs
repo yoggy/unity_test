8d7fe829361db54940f3203e595726e7ff73b964
yoggy <yoggy0@gmail.com>
2015-06-11 20:55:52 +0900

    ...

---
+++ test02/Assets/Scripts/play_bgm.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_bgm : MonoBehaviour {
+	
+	SpriteRenderer spriteRenderer;
+	AudioSource audioSource;
+
+	public Sprite[] sprites = new Sprite[2];
+
+	void Start ()
+	{
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		audioSource = GameObject.Find("bgm").GetComponent<AudioSource>();
+		Debug.Log ("Start ()");
+	}
+	
+	void Update () 
+	{
+		
+	}
+	
+	void OnMouseDown() {
+		Debug.Log ("OnMouseDown ()");
+		spriteRenderer.sprite = sprites[1];
+		audioSource.Play ();
+	}
+	
+	void OnMouseUp() {
+		Debug.Log ("OnMouseUp ()");
+		spriteRenderer.sprite = sprites[0];
+	}
+}
---
+++ test02/Assets/Scripts/play_se.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_se : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+	public Sprite[] sprites = new Sprite[2];
+
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		audioSource = gameObject.GetComponent<AudioSource> ();
+
+		Debug.Log ("Start ()");
+	}
+
+	void Update () 
+	{
+
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("OnMouseDown ()");
+		spriteRenderer.sprite = sprites[1];
+		audioSource.PlayOneShot (audioSource.clip);
+	}
+
+	void OnMouseUp() {
+		Debug.Log ("OnMouseUp ()");
+		spriteRenderer.sprite = sprites[0];
+	}
+}
