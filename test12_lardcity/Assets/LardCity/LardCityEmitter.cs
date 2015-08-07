4dc185030de4ff4bbd750c285ac374b4df6d166e
yoggy <yoggy0@gmail.com>
2015-08-07 18:58:59 +0900

    ...

---
+++ test12_lardcity/Assets/LardCity/LardCityEmitter.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class LardCityEmitter : MonoBehaviour {
+
+	public GameObject _prefab;
+
+	[Range(0.01f, 1.0f)]
+	public float _slideSpeed = 0.1f;
+
+	[Range(0, 1.0f)]
+	public float _emitRate = 0.5f;
+
+	[Range(1,30)]
+	public float _lifeTimeLimit = 10;
+
+	public float _minHeight = 7;
+	public float _maxHeight = 20;
+
+	public float _minWidth = 3;
+	public float _maxWidth = 7;
+
+	public float _minPosZ = 0;
+	public float _maxPosZ = 100;
+	
+	public int _quantizeUnit = 12;
+
+	void Update() {
+		emitLardCityCube();
+	}
+		
+	void emitLardCityCube() {
+		if (Random.Range (0.0f, 1.0f) > _emitRate)
+			return;
+
+		// 親と同じ位置にプレハブ生成
+		Vector3 parent_pos = gameObject.transform.position;
+		Quaternion parent_q = gameObject.transform.rotation;
+		GameObject obj = (GameObject)Instantiate(_prefab, parent_pos, parent_q);
+		
+		// 親を設定。このときobjの座標は親オブジェクトから差し引きされる。つまり0に戻される
+		obj.transform.parent = gameObject.transform;
+		
+		// 子独自の位置を設定
+		int z = (int)(Random.Range(_minPosZ, _maxPosZ) / _quantizeUnit) * _quantizeUnit;
+		obj.transform.Translate(0, 0, z);
+		
+		// パラメータ設定
+		LardCityParticle p = obj.GetComponent<LardCityParticle>();
+		p.lifeTimeLimit = _lifeTimeLimit;
+		p.SizeW = Random.Range (_minWidth, _maxWidth);
+		p.SizeH = Random.Range (_minHeight, _maxHeight);
+		p.dx = -_slideSpeed;
+	}
+}
---
+++ test12_lardcity/Assets/LardCity/LardCityParticle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LardCityParticle : MonoBehaviour {
+
+	public float dx = -0.1f;
+	public float dy = 0.0f;
+	public float dz = 0.0f;
+
+	float _size_w = 5.0f;
+	public float SizeW {
+		get {return _size_w;}
+		set {_size_w = value; UpdateSize();}
+	}
+
+	float _size_h = 10.0f;
+	public float SizeH {
+		get {return _size_h;}
+		set {_size_h = value; UpdateSize();}
+	}
+
+	public float lifeTimeLimit = 10.0f;
+
+	float _lifeTime = 0.0f;
+
+	void Start () {
+	}
+
+	void UpdateSize() {
+		GameObject _cube;
+		_cube = transform.FindChild ("Cube").gameObject;
+
+		_cube.transform.localScale = new Vector3 (_size_w, _size_h, _size_w);
+		_cube.transform.localPosition = new Vector3 (0, _size_h / 2.0f, 0);
+	}
+
+	void Update () {
+		_lifeTime += Time.deltaTime;
+		
+		gameObject.transform.Translate (new Vector3 (dx, dy, dz));
+		
+		if (_lifeTime > lifeTimeLimit) {
+			Destroy(gameObject, 0.5f);
+		}
+	}
+}
