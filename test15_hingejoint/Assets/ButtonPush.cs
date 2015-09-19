f7afcd31a4136b9d33e7782b10e2457dd20dba9e
yoggy <yoggy0@gmail.com>
2015-09-19 18:10:09 +0900

    ...

---
+++ test15_hingejoint/Assets/ButtonPush.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPush : MonoBehaviour {
+
+    public GameObject movingObject;
+
+    bool enableMoving;
+    float t;
+    Vector3 originalPosition;
+
+    void Start()
+    {
+        t = 0.0f;
+        originalPosition = movingObject.transform.position;
+    }
+
+    void Update()
+    {
+        if (enableMoving == false)
+        {
+            t = 0.0f;
+            movingObject.transform.position = originalPosition;
+            return;
+        }
+
+        t += Time.deltaTime;
+        movingObject.transform.Translate(0.0f, 0.1f, 0.0f);
+
+        if (t > 1.0f)
+        {
+            enableMoving = false;
+        }
+    }
+
+    public void OnClick()
+    {
+        enableMoving = true;
+    }
+}
