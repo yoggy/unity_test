373525d3226688f2cecb30057afd55fedf17dc5a
yoggy <yoggy0@gmail.com>
2015-10-15 22:48:31 +0900

    ...

---
+++ test17_sound/Assets/PressButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressButton : MonoBehaviour {
+
+    public AudioSource targetAudioSource;
+
+
+    public void OnClick()
+    {
+        targetAudioSource.Play();
+    }
+
+}
