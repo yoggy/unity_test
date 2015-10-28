faa040e835c773fed36369c3884baba18fe235df
yoggy <yoggy0@gmail.com>
2015-10-28 14:27:16 +0900

    ...

---
+++ test18_billboard/Assets/Etc/CountUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountUp : MonoBehaviour {
+
+    public TextMesh targetTextMesh;
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+        if (targetTextMesh == null) return;
+
+
+        targetTextMesh.text = string.Format("t={0:f2}", Time.time);
+    }
+}
