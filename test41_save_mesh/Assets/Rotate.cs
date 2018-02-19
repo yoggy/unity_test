f9d49abbd45d4e3ebcfe2b1c8eaa70af10939f83
yoggy <yoggy0@gmail.com>
2018-02-19 21:14:27 +0900

    ...
    ...

---
+++ test41_save_mesh/Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    [SerializeField]
+    float rx;
+
+    [SerializeField]
+    float ry;
+
+    [SerializeField]
+    float rz;
+
+	void Update () {
+        gameObject.transform.Rotate(rx, ry, rz);		
+	}
+}
---
+++ test41_save_mesh/README.md
@@ -0,0 +1,7 @@
+test41_save_mesh
+====
+スクリプトからMeshインスタンスをPrefabとして保存するサンプル
+
+![img01.gif](img01.gif)
+
+
