057a9d82a21c4f0e73bff3dd81cef38f98612605
yoggy <yoggy0@gmail.com>
2021-04-01 17:54:54 +0900

    ...

---
+++ test68_playerprefs/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField input_username;
+    public InputField input_password;
+
+    public const string KEY_USERNAME = "username";
+    public const string KEY_PASSWORD = "password";
+
+    void Start()
+    {
+        input_username.text = PlayerPrefs.GetString(KEY_USERNAME, "");
+        input_password.text = PlayerPrefs.GetString(KEY_PASSWORD, "");
+    }
+
+    public void OnPressLoginButton()
+    {
+        PlayerPrefs.SetString(KEY_USERNAME, input_username.text);
+        PlayerPrefs.SetString(KEY_PASSWORD, input_password.text);
+    }
+}
---
+++ test68_playerprefs/README.md
@@ -0,0 +1,28 @@
+# test68_playerprefs
+
+![img01.png](img01.png)
+
+## code
+```
+public class LoginManager : MonoBehaviour
+{
+    public InputField input_username;
+    public InputField input_password;
+
+    public const string KEY_USERNAME = "username";
+    public const string KEY_PASSWORD = "password";
+
+    void Start()
+    {
+        input_username.text = PlayerPrefs.GetString(KEY_USERNAME, "");
+        input_password.text = PlayerPrefs.GetString(KEY_PASSWORD, "");
+    }
+
+    public void OnPressLoginButton()
+    {
+        PlayerPrefs.SetString(KEY_USERNAME, input_username.text);
+        PlayerPrefs.SetString(KEY_PASSWORD, input_password.text);
+    }
+}
+```
+
